a61e18820d4ccf11c822a2024eb9be1514fe94cb
Sanoj <96434101+techysanoj@users.noreply.github.com>
2025-08-22 22:04:56 +0530

    understand title metadata

--- routing-demo/src/app/about/page.tsx
+++ routing-demo/src/app/about/page.tsx
@@ -1,6 +1,10 @@
-export const metadata = {
-    title: "About techysanoj",
-}
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: {
+    absolute: "Blog" // only blog irrespective of the parent metadata title pattern
+  }, 
+};
 
 export default function About() {
     return <h1> About Me</h1>
