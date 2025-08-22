0a76ecc0eb665ded63b39b495c9ae22269bdd7c9
Sanoj <96434101+techysanoj@users.noreply.github.com>
2025-08-22 20:18:20 +0530

    understand private folders

---
+++ routing-demo/src/app/%5Flib_check/page.tsx
@@ -0,0 +1,3 @@
+export default function LibCheck() {
+    return <h1> To use underscore use %5F in the folder name</h1>
+}
---
+++ routing-demo/src/app/_lib/page.tsx
@@ -0,0 +1,3 @@
+export default function PrivateRoute() {
+    return <h1>You can not view this in the browser</h1>
+}
