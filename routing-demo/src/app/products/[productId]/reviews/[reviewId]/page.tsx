271c09e2f0f0269f96d3a8e2660e0b06e90fa731
Sanoj <96434101+techysanoj@users.noreply.github.com>
2025-08-22 19:45:02 +0530

    understanding not found page with different methods

--- routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,8 +1,12 @@
+import { notFound } from "next/navigation"
 export default async function ReviewsData({params} : {
     params: Promise<{productId: string, reviewId: string}>
 }) {
     const reviewId = (await params).reviewId
     const productId = (await params).productId
+    if(parseInt(reviewId) > 100) {
+        notFound();
+    }
     return (
         <>
         <h1>Review {reviewId} about product {productId}</h1>
