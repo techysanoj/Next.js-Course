bddd699183189e97710bc642ee52c43627896b32
Sanoj <96434101+techysanoj@users.noreply.github.com>
2025-08-22 11:22:38 +0530

    dynamic route with dynamic params handler

--- routing-demo/src/app/products/[productId]/page.tsx
+++ routing-demo/src/app/products/[productId]/page.tsx
@@ -1,7 +1,10 @@
-export default function ProductList() {
+export default async function ProductDetails({params} : {
+    params: Promise<{productId: string}>
+}) {
+    const productId = (await params).productId
     return (
         <>
-        <h1>Details about product</h1>
+        <h1>Details about product {productId}</h1>
         </>
     )
-}
+} 
