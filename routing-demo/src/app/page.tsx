069fa363285c52275880247c259479388dc50305
Sanoj <96434101+techysanoj@users.noreply.github.com>
2025-08-23 10:50:58 +0530

    understanding Link Component

--- routing-demo/src/app/page.tsx
+++ routing-demo/src/app/page.tsx
@@ -1,3 +1,11 @@
+import Link from "next/link"
 export default function Home() {
-    return <h1>Home Page</h1>
+    return ( 
+    <>
+        <h1>Welcome Home</h1>
+        <Link href= "/about">About Us Page</Link>
+        <br></br>
+        <Link href= "/products">Products Us Page</Link>
+    </>
+    )
 }
--- routing-demo/src/app/products/page.tsx
+++ routing-demo/src/app/products/page.tsx
@@ -1,10 +1,14 @@
+import Link from "next/link"
 export default function ProductList() {
+    const productId = 100;
     return (
         <>
+        <Link href= "/">Back Home</Link>
         <h1>Product List</h1>
-        <h1>Product 1</h1>
-        <h1>Product 2</h1>
-        <h1>Product 3</h1>
+        <Link href="/products/1">Product 1</Link>
+        <Link href="/products/2">Product 2</Link>
+        <Link href="/products/3" replace>Product 3</Link>
+        <Link href={`/products/${productId}`}>Product {productId}</Link>
         </>
     )
 }
