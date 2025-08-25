9b126c2b1bd86848ed3c98aa0f5dcffb4cf7c6e2
Sanoj <96434101+techysanoj@users.noreply.github.com>
2025-08-25 11:08:22 +0530

    understanding error.tsx file

--- routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,9 +1,19 @@
 import { notFound, redirect} from "next/navigation";
+
+function getRandomInt(count: number) {
+    return Math.floor(Math.random() * count);
+}
+
+
 export default async function ReviewsData({params} : {
     params: Promise<{productId: string, reviewId: string}>
 }) {
     const reviewId = (await params).reviewId
     const productId = (await params).productId
+    const random = getRandomInt(2);
+    if(random == 1) {
+        throw new Error("Error Loading")
+    }
     if(parseInt(reviewId) > 100) {
         // notFound();
         redirect("/");
