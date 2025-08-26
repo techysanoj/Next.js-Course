510f5a8aa9c4aea004a6ecc6d10323d404137562
Sanoj <96434101+techysanoj@users.noreply.github.com>
2025-08-26 19:29:57 +0530

    implementing delete request

--- route-handlers-demo/src/app/comments/[id]/route.ts
+++ route-handlers-demo/src/app/comments/[id]/route.ts
@@ -21,3 +21,15 @@ export async function PATCH(
     return Response.json(comments[index])
 
 }
+
+export async function DELETE(
+    request: Request,
+    {params}: {params: Promise<{id: string}>}
+) {
+    const {id} = await params;
+    const index = comments.findIndex((comment) => comment.id === parseInt(id));
+    const deletedComment = comments[index];
+    comments.splice(index, 1)
+    return Response.json(deletedComment);
+
+}
