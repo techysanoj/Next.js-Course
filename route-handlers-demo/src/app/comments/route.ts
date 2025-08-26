57e9b45a9eb712e4f3902baf2413ca36955a022d
Sanoj <96434101+techysanoj@users.noreply.github.com>
2025-08-26 20:24:31 +0530

    understanding url query parameter

--- route-handlers-demo/src/app/comments/route.ts
+++ route-handlers-demo/src/app/comments/route.ts
@@ -1,6 +1,11 @@
+import { NextRequest } from "next/server";
 import { comments } from "./data"
-export async function GET() {
-    return Response.json(comments)
+export async function GET(request: NextRequest) {
+    const searchParams = request.nextUrl.searchParams;
+    const query = searchParams.get("query"); // single query parameter
+    // it will be like in the query you are refering to ?query=first
+    const filteredComments = query ? comments.filter((comment) => comment.text.includes(query)) : comments;
+    return Response.json(filteredComments)
 }
 
 export async function POST(request: Request) {
