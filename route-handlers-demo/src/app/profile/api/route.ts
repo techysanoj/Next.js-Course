18af83a478ead6290bd613d8aa40f94377a71831
Sanoj <96434101+techysanoj@users.noreply.github.com>
2025-08-26 21:10:49 +0530

    understand redirect api

--- route-handlers-demo/src/app/profile/api/route.ts
+++ route-handlers-demo/src/app/profile/api/route.ts
@@ -1,5 +1,6 @@
 import { type NextRequest } from "next/server";
 import { headers } from "next/headers";
+import { cookies } from "next/headers";
 
 export async function GET(request: NextRequest) {
     // const requestHeaders = new Headers(request.headers);
@@ -7,10 +8,18 @@ export async function GET(request: NextRequest) {
     const headerList = await headers(); // it is an async method
     console.log(headerList.get("Authorization"))
     // return new Response("Profile API data");
+
+    // const theme = request.cookies.get("theme");
+    // console.log(theme)
+    const cookieStore = await cookies();
+    cookieStore.set("resultsPerPage", "20");
+
+    console.log(cookieStore.get("resultsPerPage"));
     return new Response("<h1>Profile API Data</h1>",
         {
             headers: {
-            "Content-Type": "text/html" // custom headers
+            "Content-Type": "text/html",// custom headers
+            "Set-Cookie": "theme=dark",
         },
         })
 }
