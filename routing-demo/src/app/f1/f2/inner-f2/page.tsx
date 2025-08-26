b7193e62333d857f7ed00ad19ea57cdfdcdfce0a
Sanoj <96434101+techysanoj@users.noreply.github.com>
2025-08-26 12:25:41 +0530

    understanding intercepting route

---
+++ routing-demo/src/app/f1/f2/inner-f2/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link"
+
+export default function InnerF2() {
+
+    return (
+        <>
+            <h1>Inner F2 Page</h1>
+            <Link href="/f5">F5</Link>
+        </>
+    )
+}
---
+++ routing-demo/src/app/f1/page.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link"
+
+export default function F1() {
+    return (
+        <>
+            <h1>F1 Page</h1>
+            <div>
+                <Link href="/f1/f2">F2</Link> 
+                {/* normal behaviour he
+                re f1 is source folder and target folder is f2.
+                 */}
+                 <br></br>
+                 <br></br>
+                <Link href="/f3">F3</Link> 
+            </div>
+        </>
+    )
+}
