16978de66f43b5a1e7f3fbde603993aaf2f323e3
Sanoj <96434101+techysanoj@users.noreply.github.com>
2025-08-26 18:41:45 +0530

    implementing dynamic route handlers

---
+++ route-handlers-demo/src/app/comments/[id]/route.ts
@@ -0,0 +1,9 @@
+import { comments } from "../data";
+
+export async function GET(_request: Request,
+    {params}: {params: Promise<{id: string}>}) {
+    
+    const {id} = await params
+    const comment = comments.find((comment) => comment.id === parseInt(id));
+    return Response.json(comment)
+}
