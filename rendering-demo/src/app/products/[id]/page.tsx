cc83380277ee2ea34569493c890e388e131cc473
Sanoj <panditsanoj124@gmail.com>
2025-08-28 21:02:46 +0530

    understanding dynamic params

--- rendering-demo/src/app/products/[id]/page.tsx
+++ rendering-demo/src/app/products/[id]/page.tsx
@@ -1,3 +1,5 @@
+export const dynamicParams = false; // false - this tells that whatever route which is not in pre rendered list it will give 404 error
+
 // for pre rendering pages which we are showing in the product section
 export async function generateStaticParams() {
     return [{id: "1"}, {id: "2"}, {id: "3"}];
