0c0c4b5ee74dd96c30b1538ca1da0b1f06a2ce04
Sanoj <panditsanoj124@gmail.com>
2025-08-28 20:05:48 +0530

    understanding static rendering properly by using npm run build and npm run start

--- rendering-demo/src/app/page.tsx
+++ rendering-demo/src/app/page.tsx
@@ -14,6 +14,7 @@ export default function Home() {
           priority
         />
         <Link href="/dashboard">Dashboard</Link>
+        <Link href="/about">About</Link>
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
