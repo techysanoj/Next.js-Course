26705999167ba53f7150b1944ddc00e89c196376
Sanoj <panditsanoj124@gmail.com>
2025-08-28 20:33:06 +0530

    understand dynamic rendering

--- rendering-demo/src/app/about/page.tsx
+++ rendering-demo/src/app/about/page.tsx
@@ -1,8 +1,12 @@
-import { useState } from "react"
+// import { useState } from "react"
 
-export default function AboutPage() {
+import { cookies } from "next/headers"
+
+export default async function AboutPage() {
     console.log("About server components") // it will show server (then the msg) tells it is a server component by default
     // const [name, setName] = useState(""); //-> we will get the error while using this because it is a server component
-
+    const cookieStore = await cookies();
+    const theme = cookieStore.get("theme");
+    console.log(theme);
     return <h1>About Page {new Date(). toLocaleDateString()}</h1>
 }
