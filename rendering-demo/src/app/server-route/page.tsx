28830c886ac0b9b85897b46f4e52aa84a358acd3
Sanoj <panditsanoj124@gmail.com>
2025-08-29 11:01:17 +0530

    understanding third party packages server component

--- rendering-demo/src/app/server-route/page.tsx
+++ rendering-demo/src/app/server-route/page.tsx
@@ -1,6 +1,43 @@
-import { serverSideFunction } from "@/utils/server-utils"
+import { serverSideFunction } from "@/utils/server-utils";
+// import React from 'react';
+// import Slider from 'react-slick';
+// import 'slick-carousel/slick/slick.css';
+// import 'slick-carousel/slick/slick-theme.css';
+
+import {ImageSlider} from "@/components/ImageSlider";
 
 export default function ServerRoutePage() {
     const result = serverSideFunction();
-    return <h1>Server Route {result}</h1>
+    // return <h1>Server Route {result}</h1>
+
+    // using this we can get errors
+    // const settings = {
+    //     dots: true,
+    //   };
+    //   return (
+    //     <div className="image-slider-container">
+    //       <Slider {...settings}>
+    //         <div>
+    //           <img src="https://picsum.photos/g/400/200" />
+    //         </div>
+    //         <div>
+    //           <img src="https://picsum.photos/g/400/200" />
+    //         </div>
+    //         <div>
+    //           <img src="https://picsum.photos/g/400/200" />
+    //         </div>
+    //         <div>
+    //           <img src="https://picsum.photos/g/400/200" />
+    //         </div>
+    //       </Slider>
+    //     </div>
+    //   );
+
+    // so to use the common function which is beign used in the client side use the component method
+    return (
+        <>
+        <h1>Server Route {result}</h1>
+        <ImageSlider />
+        </>
+    )
 }
--- rendering-demo/src/components/ImageSlider.tsx
+++ rendering-demo/src/components/ImageSlider.tsx
@@ -1,16 +1,11 @@
-"use client"
-
-// import { serverSideFunction } from "@/utils/server-utils"; // using this directly doess not prevent to use server funciton in client side code
-// to prevent this we need to use server-only package. Install this using npm i server-only
+"use client";
 
 import React from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
-export default function ClientRoutePage() {
-    // const result = serverSideFunction();
-    // return <h1>Client Route {result}</h1>
+export const ImageSlider = () => {
     const settings = {
         dots: true,
       };
