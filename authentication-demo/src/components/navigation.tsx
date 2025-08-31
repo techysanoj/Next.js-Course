010f1f48331716a03713cd6a1ee1609699a37583
Sanoj <panditsanoj124@gmail.com>
2025-08-31 21:17:43 +0530

    understanding authentication with clerk setup

---
+++ authentication-demo/src/components/navigation.tsx
@@ -0,0 +1,21 @@
+import { SignInButton , SignOutButton} from "@clerk/nextjs"
+export const Navigation = () => {
+    return (
+        <nav className="bg-[var(--background)] border-b border-[var(--foreground)]/10 cursor-pointer">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div className="flex justify-between h-16 items-center">
+                    <div className="flex-shrink-0">
+                        <h1 className="text-xl font-semibold text-[var(--foreground)]">
+                            Next.JS App
+                        </h1>
+                    </div>
+                    <div className="flex items-center gap-4">
+                        <SignInButton mode="modal"></SignInButton>
+                        <SignOutButton></SignOutButton>
+                    </div>
+                </div>
+            </div>
+
+        </nav>
+    )
+}
