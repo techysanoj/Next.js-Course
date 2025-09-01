6ff1bd98075451d3da4ed4eee98e92b101f262c2
Sanoj <panditsanoj124@gmail.com>
2025-09-01 19:53:41 +0530

    understanding protecting routes using clerk with advance options

--- authentication-demo/src/middleware.ts
+++ authentication-demo/src/middleware.ts
@@ -1,6 +1,19 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-export default clerkMiddleware();
+// const isProtectedRoute =  createRouteMatcher("/user-profile")
+const isPublicRoute = createRouteMatcher(["/", "/sign-in(.*)", "/sign-up(.*)"]);
+
+export default clerkMiddleware(async (auth, req) => {
+  // if(isPublicRoute(req)) await auth.protect();
+
+  const {userId, redirectToSignIn}= await auth()
+  if(userId && !isPublicRoute(req)) {
+
+    // add custom logic
+    return redirectToSignIn();
+  }
+
+});
 
 export const config = {
   matcher: [
